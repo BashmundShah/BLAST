b25b0dea8b2e3d9c2dcf492c9d0b247f0ab04cb6
Bashmund Shah <bashmundkhan@yahoo.com>
2019-11-30 18:03:39 +0500

    reading file and making subsets: phase 2

--- makeBlastDB.py
+++ makeBlastDB.py
@@ -1,18 +1,26 @@
-import re
+import pandas as pd
+
+df = pd.DataFrame(columns=['Sequence', 'SeqName', 'Position', 'Kmer_Size'])
+
 def readfile(filename,k):
     file=open(filename)
     text=file.read()
     text=text.split(">")
     text.remove('')
+    i=1
     for x in text:
         seqName,sequence=x.split("\n",maxsplit=1)
-        print(seqName,sequence)
+        sequence=sequence.replace('\n','')
+        print(i,sequence)
+        i+=1
         makeSubsets(seqName,sequence,k)
 
 def makeSubsets(seqName,sequence,k):
     for position in range(0,len(sequence)-k+1):
-        print(seqName,sequence[position:position+k],position)
+        subsequence=sequence[position:position+k]
+        df.loc[len(df)+1]=[subsequence,seqName,position,k]
 if __name__ == '__main__':
-    k=5
-    filename='basic test seq.txt'
+    k=7
+    filename='TEST Seq.txt'
     readfile(filename,k)
+    print(df)
