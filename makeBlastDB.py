78937c7ce2136dbb0f283d75519866f2c62c60c9
Bashmund Shah <bashmundkhan@yahoo.com>
2019-11-30 17:40:28 +0500

    reading file and making subsets: phase 1

--- makeBlastDB.py
+++ makeBlastDB.py
@@ -1,2 +1,18 @@
+import re
+def readfile(filename,k):
+    file=open(filename)
+    text=file.read()
+    text=text.split(">")
+    text.remove('')
+    for x in text:
+        seqName,sequence=x.split("\n",maxsplit=1)
+        print(seqName,sequence)
+        makeSubsets(seqName,sequence,k)
+
+def makeSubsets(seqName,sequence,k):
+    for position in range(0,len(sequence)-k+1):
+        print(seqName,sequence[position:position+k],position)
 if __name__ == '__main__':
-    print('starting the project')
+    k=5
+    filename='basic test seq.txt'
+    readfile(filename,k)
