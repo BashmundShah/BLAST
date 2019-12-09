1935c1144b001d048849bc9be25d7b8175c9a9d9
Bashmund Shah <bashmundkhan@yahoo.com>
2019-12-09 22:41:33 +0500

    blast on query sequence completed

--- BLASTonQuerySequence.py
+++ BLASTonQuerySequence.py
@@ -1,36 +1,82 @@
-import makeBlastDB as bdb
 import pandas as pd
-qdf=pd.DataFrame(columns=['Sequence','SeqName','Position','Kmer_Size'])
-def createQueryDF(filename,k):
-    file=open(filename)
-    names=[]
-    name=''
-    sequence=''
-    line=file.readline()
-    while line:
-        if(line[0]==">"):
-            createQueryKmers(name,sequence,k)
-            name=line[1:-1]
-            names.append(name)
-            sequence=''
+
+qdf = pd.DataFrame(columns=['Sequence', 'SeqName', 'Position', 'Kmer_Size'])
+id = 1
+
+
+def processDataframe(df):
+    output = pd.DataFrame(columns=['S.no', 'QuerySeqID', '#ofMatchingKmers', 'startPosX',
+                                   'endPosX', 'TotalLenX', 'DatabaseSeqID', 'startPosY',
+                                   'EndPosY', 'TotalLenY'])
+    n = len(df)
+    print(qdf)
+    nKmers = 1
+    startPos_x = -1
+    lastPos_x = 0
+    startPos_y = -1
+    lastPos_y = 0
+    for i in range(1, n):
+        if (startPos_x < 0) & (startPos_y < 0):
+            startPos_x = df['Position_x'].iloc[i - 1]
+            startPos_y = df['Position_y'].iloc[i - 1]
+        if (df['Position_x'].iloc[i] == df['Position_x'].iloc[i - 1] + 1) and (
+                df['Position_y'].iloc[i] == df['Position_y'].iloc[i - 1] + 1):
+            nKmers += 1
         else:
-            sequence+=line
-        line=file.readline()
+            lastPos_x = df['Position_x'].iloc[i - 1] + k - 1
+            lastPos_y = df['Position_y'].iloc[i - 1] + k - 1
+            global id
+            output.loc[len(output) + 1] = [id, df['SeqName_x'].iloc[i - 1], nKmers, startPos_x, lastPos_x,
+                                           nKmers + k - 1, df['SeqName_y'].iloc[i - 1], startPos_y,
+                                           lastPos_y, nKmers + k - 1]
+            id += 1
+            startPos_x = df['Position_x'].iloc[i]
+            startPos_y = df['Position_y'].iloc[i]
+            lastPos_x = 0
+            lastPos_y = 0
+            nKmers = 1
+            if(i==n-1):
+                lastPos_x = df['Position_x'].iloc[i] + k - 1
+                lastPos_y = df['Position_y'].iloc[i] + k - 1
+                output.loc[len(output) + 1] = [id, df['SeqName_x'].iloc[i], nKmers, startPos_x, lastPos_x,
+                                               nKmers + k - 1, df['SeqName_y'].iloc[i], startPos_y,
+                                               lastPos_y, nKmers + k - 1]
+    output.to_csv('OutputFiles\outputFile_'+str(k)+'.csv', index=0)
+
+
+def createQueryDF(filename, k):
+    file = open(filename)
+    names = []
+    name = ''
+    sequence = ''
+    line = file.readline()
+    while line:
+        if (line[0] == ">"):
+            createQueryKmers(name, sequence, k)
+            name = line[1:-1]
+            names.append(name)
+            sequence = ''
+        else:
+            sequence += line
+        line = file.readline()
     createQueryKmers(name, sequence, k)
     file.close()
     return names
 
-def createQueryKmers(name,sequence,k):
-    sequence=sequence.replace('\n','')
-    for position in range(0,len(sequence)-k+1):
-        subsequence=sequence[position:position+k]
-        qdf.loc[len(qdf)+1]=[subsequence,name,position,k]
+
+def createQueryKmers(name, sequence, k):
+    sequence = sequence.replace('\n', '')
+    for position in range(0, len(sequence) - k + 1):
+        subsequence = sequence[position:position + k]
+        qdf.loc[len(qdf) + 1] = [subsequence, name, position+1, k]
+
+
 if __name__ == '__main__':
-    k=3
-    queryFile="querySequence.txt"
-    names=createQueryDF(queryFile,3)
-    for chunk in pd.read_csv('Database.csv',chunksize=5000,index_col=0):
-        db=chunk[chunk['SeqName']!=names[0]]
-        qdf=pd.merge(qdf,db,on="Sequence")
-    qdf=qdf.sort_values(['SeqName_y','Position_x'])
-    qdf.to_csv('testresult.csv',index=0)
+    k = 4
+    queryFile = "querySequence.txt"
+    names = createQueryDF(queryFile, k)
+    for chunk in pd.read_csv("Database/Database_" + str(k) + ".csv", chunksize=5000, index_col=0):
+        db = chunk[chunk['SeqName'] != names[0]]
+        qdf = pd.merge(qdf, db, on=['Sequence'])
+        qdf = qdf.sort_values(['SeqName_x', 'SeqName_y'])
+        processDataframe(qdf)
--- makeBlastDB.py
+++ makeBlastDB.py
@@ -1,10 +1,23 @@
+import os.path as path
+k=7
 def file_len(fname):
+    i=0
     with open(fname) as f:
         for i, l in enumerate(f):
             pass
     return i + 1
-id=file_len("Database.csv")
+id=0
+try:
+    id=file_len("Database/Database_"+str(k)+".csv")
+except IOError:
+    id=0
+
 def readFile(filename,k):
+    if path.exists("Database/Database_"+str(k)+".csv") is False:
+        database = open("Database/Database_" + str(k) + ".csv", mode='a')
+        line = "ID,Sequence,SeqName,Position\n"
+        database.write(line)
+        database.close()
     queryFile = open(filename)
     line = queryFile.readline()
     name = ''
@@ -24,16 +37,15 @@ def readFile(filename,k):
 def makeSubsets(seqName,sequence,k):
     global id
     sequence=sequence.replace('\n','')
-    database=open("Database.csv",mode='a')
+    database=open("Database/Database_"+str(k)+".csv",mode='a')
     for position in range(0,len(sequence)-k+1):
         subsequence=sequence[position:position+k]
-        line=str(id)+','+subsequence+','+seqName+','+str(position)+','+str(k)+'\n'
+        line=str(id)+','+subsequence+','+seqName+','+str(position+1)+'\n'
         database.write(line)
         id+=1
     database.close()
 
 if __name__ == '__main__':
-    k=int(input("Enter the size of K-mer:"))
     filename='TEST Seq.txt'
     readFile(filename,k)
     print("Database updated successfully")
